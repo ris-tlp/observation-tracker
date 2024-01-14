d900ac15bd5c5054e444c1b3ee38576b3631202d
ris-tlp <omarkhantlp@gmail.com>
2024-01-14 02:15:14 -0500

    setup persistent h2

--- src/main/java/com/observatory/observationscheduler/ObservationSchedulerApplication.java
+++ src/main/java/com/observatory/observationscheduler/ObservationSchedulerApplication.java
@@ -2,12 +2,15 @@ package com.observatory.observationscheduler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class ObservationSchedulerApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(ObservationSchedulerApplication.class, args);
+		new SpringApplicationBuilder()
+				.profiles("dev").
+				sources(ObservationSchedulerApplication.class)
+				.run(args);
 	}
 
 }
