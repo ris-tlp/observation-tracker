b60d2785dc354c64e3bbdbde594279f6d1ea9d50
ris-tlp <omarkhantlp@gmail.com>
2024-01-18 01:30:38 -0500

    add testing data for observations

--- src/main/java/com/observatory/observationscheduler/startup/DataInit.java
+++ src/main/java/com/observatory/observationscheduler/startup/DataInit.java
@@ -1,5 +1,7 @@
 package com.observatory.observationscheduler.startup;
 
+import com.observatory.observationscheduler.observation.Observation;
+import com.observatory.observationscheduler.observation.ObservationRepository;
 import com.observatory.observationscheduler.useraccount.UserAccount;
 import com.observatory.observationscheduler.useraccount.UserAccountRepository;
 import org.slf4j.Logger;
@@ -8,19 +10,28 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
-public class DataInit implements CommandLineRunner{
+public class DataInit implements CommandLineRunner {
     private static final Logger log = LoggerFactory.getLogger(DataInit.class);
     private final UserAccountRepository userRepo;
+    private final ObservationRepository observationRepository;
 
-    public DataInit(UserAccountRepository userRepo) {
+    public DataInit(UserAccountRepository userRepo, ObservationRepository observationRepository) {
+        this.observationRepository = observationRepository;
         this.userRepo = userRepo;
     }
 
     @Override
     public void run(String... args) throws Exception {
-
+        observationRepository.deleteAll();
         userRepo.deleteAll();
+
         userRepo.save(new UserAccount("name", "emaila"));
-        userRepo.save(new UserAccount("newName", "newEmail"));
+        UserAccount user = new UserAccount("newName", "newEmail");
+        userRepo.save(user);
+
+        observationRepository.save(new Observation("new observation", user));
+        observationRepository.save(new Observation("new new", user));
+
+
     }
 }
