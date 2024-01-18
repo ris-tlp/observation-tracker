8421db5f376139de959e1b92ab2416ffbb20d558
ris-tlp <omarkhantlp@gmail.com>
2024-01-17 20:55:55 -0500

    added data init class for testing

---
+++ src/main/java/com/observatory/observationscheduler/startup/DataInit.java
@@ -0,0 +1,24 @@
+package com.observatory.observationscheduler.startup;
+
+import com.observatory.observationscheduler.useraccount.UserAccount;
+import com.observatory.observationscheduler.useraccount.UserAccountRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataInit implements CommandLineRunner{
+    private static final Logger log = LoggerFactory.getLogger(DataInit.class);
+    private final UserAccountRepository userRepo;
+
+    public DataInit(UserAccountRepository userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+//        userRepo.save(new UserAccount("name", "emaila"));
+//        userRepo.save(new UserAccount("newName", "newEmail"));
+    }
+}
