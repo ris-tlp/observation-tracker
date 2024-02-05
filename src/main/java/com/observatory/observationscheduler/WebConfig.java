feb3fb7a3f664f8081bdc9f6ea6b674d3edfea55
ris-tlp <omarkhantlp@gmail.com>
2024-02-05 15:03:36 -0500

    add enum converter to mvc config

---
+++ src/main/java/com/observatory/observationscheduler/WebConfig.java
@@ -0,0 +1,14 @@
+package com.observatory.observationscheduler;
+
+import com.observatory.observationscheduler.celestialevent.StatusEnumConverter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new StatusEnumConverter());
+    }
+}
