b9d727ef1b3c3f51de76da190d376b49b10af107
ris-tlp <omarkhantlp@gmail.com>
2024-02-06 15:38:48 -0500

    Provision S3 bucket and store name in parameter store

---
+++ infrastructure/s3.tf
@@ -0,0 +1,17 @@
+resource "aws_s3_bucket" "observation-tracker-bucket" {
+  bucket_prefix = var.bucket_prefix
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_versioning" "audiophile-bucket-versioning" {
+  bucket = aws_s3_bucket.observation-tracker-bucket.id
+  versioning_configuration {
+    status = var.versioning
+  }
+}
+
+resource "aws_ssm_parameter" "s3_secret" {
+  name = "/production/blob/uri"
+  type = "SecureString"
+  value = aws_s3_bucket.observation-tracker-bucket.id
+}
---
+++ infrastructure/variable.tf
@@ -0,0 +1,15 @@
+variable "aws_region" {
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "bucket_prefix" {
+  description = "Bucket prefix for the S3"
+  type        = string
+  default     = "observation-tracker-"
+}
+
+variable "versioning" {
+  type    = string
+  default = "Enabled"
+}
