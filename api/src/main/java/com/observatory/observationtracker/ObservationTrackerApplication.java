81da9c9de1b31d40c8d380b4bc9dd05eaa2627a7
ris-tlp <omarkhantlp@gmail.com>
2024-02-26 02:24:25 -0500

    Proper profile configurations

--- api/src/main/java/com/observatory/observationtracker/ObservationTrackerApplication.java
+++ api/src/main/java/com/observatory/observationtracker/ObservationTrackerApplication.java
@@ -3,12 +3,14 @@ package com.observatory.observationtracker;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
+// @TODO: Figure out profiles otherwise it's too dirty
+// @TODO: Dockerfile
 @SpringBootApplication
 public class ObservationTrackerApplication {
     public static void main(String[] args) {
         new SpringApplicationBuilder()
                 .sources(ObservationTrackerApplication.class)
-                .profiles("dev")
+//                .profiles("dev")
                 .run(args);
     }
 
