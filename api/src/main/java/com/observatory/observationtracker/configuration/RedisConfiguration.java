c53eb7406b432c60562449adc9d3c7f0b97bbb16
Omar <omarkhantlp@gmail.com>
2024-03-05 20:19:11 -0500

    Redis cache initial configuration

---
+++ api/src/main/java/com/observatory/observationtracker/configuration/RedisConfiguration.java
@@ -0,0 +1,20 @@
+package com.observatory.observationtracker.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+
+@Configuration
+public class RedisConfiguration {
+    @Bean
+    public RedisCacheConfiguration cacheConfiguration() {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(60))
+                .disableCachingNullValues()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+    }
+}
